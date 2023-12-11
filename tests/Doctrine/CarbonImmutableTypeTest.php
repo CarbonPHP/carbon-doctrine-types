99f76ffa36cce3b70a4a6abce41dba15ca2e84cb
kylekatarnls <kylekatarnls@gmail.com>
2023-12-11 18:09:12 +0100

    Cap precision depending on maximum per platform

--- tests/Doctrine/CarbonImmutableTypeTest.php
+++ tests/Doctrine/CarbonImmutableTypeTest.php
@@ -7,7 +7,10 @@ namespace Doctrine;
 use Carbon\CarbonImmutable;
 use Carbon\Doctrine\CarbonImmutableType;
 use DateTimeImmutable;
+use Doctrine\DBAL\Platforms\DB2Platform;
 use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Platforms\OraclePlatform;
+use Doctrine\DBAL\Platforms\SqlitePlatform;
 use Doctrine\DBAL\Types\Type;
 use PHPUnit\Framework\TestCase;
 
@@ -24,9 +27,7 @@ class CarbonImmutableTypeTest extends TestCase
 
     public function testCarbonImmutableType(): void
     {
-        Type::addType('carbon_immutable', CarbonImmutableType::class);
-        /** @var CarbonImmutableType $type */
-        $type = Type::getType('carbon_immutable');
+        $type = self::getType();
         $platformClass = class_exists(MySQLPlatform::class)
             ? MySQLPlatform::class
             : \Doctrine\DBAL\Platforms\MySqlPlatform::class;
@@ -48,5 +49,38 @@ class CarbonImmutableTypeTest extends TestCase
 
         self::assertInstanceOf(CarbonImmutable::class, $date);
         self::assertSame('2000-01-01 12:00:00 UTC', $date->format('Y-m-d H:i:s e'));
+    }
+
+    public function testPrecisionPerPlatform(): void
+    {
+        $type = self::getType();
+
+        self::assertSame('DATETIME(3)', $type->getSQLDeclaration([
+            'precision' => 99,
+        ], new SqlitePlatform()));
+
+        self::assertSame('DATETIME(6)', $type->getSQLDeclaration([
+            'precision' => 99,
+        ], new MySQLPlatform()));
+
+        self::assertSame('TIMESTAMP(9)', $type->getSQLDeclaration([
+            'precision' => 99,
+        ], new OraclePlatform()));
+
+        self::assertSame('TIMESTAMP(12)', $type->getSQLDeclaration([
+            'precision' => 99,
+        ], new DB2Platform()));
+    }
+
+    private static function getType(): CarbonImmutableType
+    {
+        if (!Type::hasType('carbon_immutable')) {
+            Type::addType('carbon_immutable', CarbonImmutableType::class);
+        }
+
+        /** @var CarbonImmutableType $type */
+        $type = Type::getType('carbon_immutable');
+
+        return $type;
     }
 }
