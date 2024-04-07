a15b89e1c4cf15df22128b009be24605298d1217
varshneydevansh <varshney.devansh614@gmail.com>
2024-04-07 18:24:45 +0530

    #2835 Fix time-zone-aware doctrine types

---
+++ src/Carbon/Doctrine/CarbonTzType.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Carbon\Doctrine;
+
+use Carbon\Carbon;
+use Carbon\CarbonInterface;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\DateTimeTzType;
+
+class CarbonTzType extends DateTimeTzType
+{
+    use CarbonTypeConverter;
+
+    public function getName(): string
+    {
+        return 'carbontz';
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): ?string
+    {
+        return $this->convertCarbonToDatabaseValue($value, $platform);
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform): ?CarbonInterface
+    {
+        return $this->convertToCarbon($value, $platform);
+    }
+
+    protected function getClassName(): string
+    {
+        return Carbon::class;
+    }
+}
---
+++ tests/Doctrine/CarbonTzTypeTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Carbon\Tests\Doctrine;
+
+use Carbon\Carbon;
+use Carbon\CarbonInterface;
+use Carbon\Doctrine\CarbonTzType;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+use PHPUnit\Framework\TestCase;
+
+class CarbonTzTypeTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        Type::addType('carbontz', CarbonTzType::class);
+    }
+
+    public function testGetName(): void
+    {
+        $type = Type::getType('carbontz');
+        $this->assertSame('carbontz', $type->getName());
+    }
+
+    public function testConvertToDatabaseValue(): void
+    {
+        $type = Type::getType('carbontz');
+        $platform = $this->createMock(AbstractPlatform::class);
+        $platform->method('getDateTimeTzFormatString')->willReturn('Y-m-d H:i:s.u P');
+
+        $carbon = Carbon::parse('2023-06-08 12:34:56.789000', 'Europe/Paris');
+        $expectedDatabaseValue = '2023-06-08 12:34:56.789000 +02:00';
+
+        $actualDatabaseValue = $type->convertToDatabaseValue($carbon, $platform);
+
+        $this->assertSame($expectedDatabaseValue, $actualDatabaseValue);
+    }
+
+    public function testConvertToPHPValue(): void
+    {
+        $type = Type::getType('carbontz');
+        $platform = $this->createMock(AbstractPlatform::class);
+
+        $databaseValue = '2023-06-08 12:34:56.789000 +02:00';
+        $expectedCarbon = Carbon::parse('2023-06-08 12:34:56.789000', 'Europe/Paris');
+
+        $actualCarbon = $type->convertToPHPValue($databaseValue, $platform);
+
+        $this->assertEquals($expectedCarbon, $actualCarbon);
+    }
+
+    public function testGetClassName(): void
+    {
+        $type = Type::getType('carbontz');
+        $this->assertSame(Carbon::class, $type->getClassName());
+    }
+}
