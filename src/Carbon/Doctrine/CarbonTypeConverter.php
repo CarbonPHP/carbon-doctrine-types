9c27e8096fd471c23e44fc25da55db6f757ca975
kylekatarnls <kylekatarnls@gmail.com>
2026-09-06 16:06:00 +0200

    Check $value is an object before checking its class

--- src/Carbon/Doctrine/CarbonTypeConverter.php
+++ src/Carbon/Doctrine/CarbonTypeConverter.php
@@ -72,7 +72,7 @@ trait CarbonTypeConverter
     {
         $class = $this->getCarbonClassName();
 
-        if ($value === null || is_a($value, $class)) {
+        if ($value === null || (is_object($value) && is_a($value, $class))) {
             return $value;
         }
 
